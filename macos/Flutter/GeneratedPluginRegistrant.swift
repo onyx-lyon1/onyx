261e8a10674c4d5f756a1b7859f7ecb44a65efbe
Eymeric <hatchchien@protonmail.com>
2022-10-20 10:11:40 +0200

    small deps update

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,7 @@
 import FlutterMacOS
 import Foundation
 
-import device_info_plus_macos
+import device_info_plus
 import flutter_local_notifications
 import mobile_scanner
 import path_provider_macos
