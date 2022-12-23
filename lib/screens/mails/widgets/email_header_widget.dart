bd2ebb8fdddd7953ca14a7ce6e5bfc3e7e42383a
Eymeric Dechelette <hatchchien@protonmail.com>
2022-12-23 12:37:49 +0100

    fix mail alignement

--- lib/screens/mails/widgets/email_header_widget.dart
+++ lib/screens/mails/widgets/email_header_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:oloid2/core/res.dart';
 import 'package:oloid2/screens/mails/mails_export.dart';
 import 'package:sizer/sizer.dart';
 
@@ -12,17 +11,14 @@ class EmailHeaderWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Theme.of(context).cardTheme.color,
-      height: Res.bottomNavBarHeight,
-      child: Container(
-        margin: const EdgeInsets.all(10),
-        decoration: BoxDecoration(
-            color: Theme.of(context).backgroundColor,
-            borderRadius: BorderRadius.circular(100)),
-        width: 85.w,
-        height: Res.bottomNavBarHeight,
-        child: Padding(
-          padding: EdgeInsets.only(left: 3.w),
+      margin: EdgeInsets.all(2.w),
+      decoration: BoxDecoration(
+        color: Theme.of(context).backgroundColor,
+        borderRadius: BorderRadius.circular(100),
+      ),
+      child: Padding(
+        padding: EdgeInsets.only(left: 3.w),
+        child: Center(
           child: TextField(
             onChanged: (String query) {
               context.read<EmailCubit>().filter(filter: query);
