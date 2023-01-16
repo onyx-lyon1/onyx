98cd414b93541fe75be74047a951b725e5653e76
Eymeric Dechelette <hatchchien@protonmail.com>
2023-01-16 11:10:55 +0100

    untested bugfix
    Former-commit-id: c67ed9755b0d552aa9c8d9b9fa4fc567387ec3b6

--- lib/screens/tomuss/states/tomuss_state.dart
+++ lib/screens/tomuss/states/tomuss_state.dart
@@ -8,12 +8,34 @@ class TomussState {
   final List<SemestreModel> semesters;
   final int currentSemesterIndex;
 
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TomussState &&
+          runtimeType == other.runtimeType &&
+          status == other.status &&
+          teachingUnits == other.teachingUnits &&
+          semesters == other.semesters &&
+          currentSemesterIndex == other.currentSemesterIndex;
+
+  @override
+  int get hashCode =>
+      status.hashCode ^
+      teachingUnits.hashCode ^
+      semesters.hashCode ^
+      currentSemesterIndex.hashCode;
+
   TomussState({
     this.status = TomussStatus.initial,
     this.teachingUnits = const [],
     this.semesters = const [],
     this.currentSemesterIndex = 0,
   });
+
+  @override
+  String toString() {
+    return 'TomussState{status: $status, teachingUnits: $teachingUnits, semesters: $semesters, currentSemesterIndex: $currentSemesterIndex}';
+  }
 
   TomussState copyWith({
     TomussStatus? status,
