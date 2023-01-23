6203198130b0ec9c2c18b41f8db24eb139825d39
Eymeric Dechelette <hatchchien@protonmail.com>
2023-01-23 16:03:21 +0100

    lock on vertical
    Former-commit-id: 8728090b4e23e1c3c08272782797549f0c2d0000

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:onyx/app.dart';
 import 'package:onyx/core/initialisations/initialisations_export.dart';
 import 'package:onyx/screens/notifications/notifications_export.dart';
@@ -13,6 +14,10 @@ import 'package:workmanager/workmanager.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown
+  ]);
   // debugRepaintRainbowEnabled = true;
   // debugRepaintTextRainbowEnabled = true;
 
