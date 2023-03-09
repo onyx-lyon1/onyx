6a08631a13d149797a3a748c62c6322fc10c0633
Eymeric Dechelette <hatchchien@protonmail.com>
2023-03-09 15:02:39 +0100

    new settings

--- lib/screens/settings/widgets/draggable_zone_widget.dart
+++ lib/screens/settings/widgets/draggable_zone_widget.dart
@@ -3,176 +3,203 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:onyx/core/res.dart';
 import 'package:onyx/screens/settings/settings_export.dart';
+import 'package:sizer/sizer.dart';
 
 class DraggableZoneWidget extends StatelessWidget {
   const DraggableZoneWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    List<DragAndDropList> contents = [
-      DragAndDropList(
-          canDrag: false,
-          decoration: BoxDecoration(
+    return BlocBuilder<SettingsCubit, SettingsState>(
+      builder: (context, state) {
+        List<DragAndDropList> contents = [
+          DragAndDropList(
+              canDrag: false,
+              decoration: BoxDecoration(
+                color: Theme.of(context).cardColor,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              contentsWhenEmpty: const SizedBox.shrink(),
+              header: const Padding(
+                padding: EdgeInsets.only(
+                  top: 15.0,
+                  left: 15,
+                  right: 15,
+                  bottom: 5,
+                ),
+                child: Text(
+                  'Activé',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+              ),
+              children: context
+                  .read<SettingsCubit>()
+                  .state
+                  .settings
+                  .enabledFunctionalities
+                  .map((e) => screenSettingsDragAndDropItem(e))
+                  .toList()),
+          DragAndDropList(
+              canDrag: false,
+              contentsWhenEmpty: const SizedBox.shrink(),
+              decoration: BoxDecoration(
+                color: Theme.of(context).cardColor,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              header: const Padding(
+                padding: EdgeInsets.only(
+                  top: 15.0,
+                  left: 15,
+                  right: 15,
+                  bottom: 5,
+                ),
+                child: Text(
+                  'Désactivé',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+              ),
+              children: context
+                  .read<SettingsCubit>()
+                  .state
+                  .settings
+                  .disabledFunctionalities
+                  .map((e) => screenSettingsDragAndDropItem(e))
+                  .toList())
+        ];
+        return DragAndDropLists(
+          listPadding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          disableScrolling: true,
+          itemDivider: const Divider(
+            thickness: 2,
+            height: 2,
+            color: Colors.transparent,
+          ),
+          itemDecorationWhileDragging: BoxDecoration(
             color: Theme.of(context).cardColor,
             borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.5),
+                spreadRadius: 2,
+                blurRadius: 3,
+                offset: const Offset(0, 0), // changes position of shadow
+              ),
+            ],
           ),
-          header: const Padding(
-            padding: EdgeInsets.only(
-              top: 15.0,
-              left: 15,
-              right: 15,
-              bottom: 5,
-            ),
-            child: Text(
-              'Active',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-            ),
-          ),
-          children: context
-              .read<SettingsCubit>()
-              .state
-              .settings
-              .enabledFunctionalities
-              .map((e) => screenSettingsDragAndDropItem(e))
-              .toList()),
-      DragAndDropList(
-          canDrag: false,
-          decoration: BoxDecoration(
-            color: Theme.of(context).cardColor,
-            borderRadius: BorderRadius.circular(10),
-          ),
-          header: const Padding(
-            padding: EdgeInsets.only(
-              top: 15.0,
-              left: 15,
-              right: 15,
-              bottom: 5,
-            ),
-            child: Text(
-              'Désactivé',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+          itemDragOnLongPress: true,
+          itemDragHandle: DragHandle(
+            verticalAlignment: DragHandleVerticalAlignment.top,
+            child: Padding(
+              padding: EdgeInsets.only(right: 10, top: 2.25.h),
+              child: Icon(
+                Icons.drag_indicator_rounded,
+                color: Theme.of(context).primaryColor,
+              ),
             ),
           ),
-          children: context
-              .read<SettingsCubit>()
-              .state
-              .settings
-              .disabledFunctionalities
-              .map((e) => screenSettingsDragAndDropItem(e))
-              .toList())
-    ];
+          verticalAlignment: CrossAxisAlignment.center,
+          children: contents,
+          lastListTargetSize: 0.0,
+          onItemReorder: (int oldItemIndex, int oldListIndex, int newItemIndex,
+              int newListIndex) {
+            if (oldListIndex == 0 && newListIndex == 1) {
+              Functionalities item = context
+                  .read<SettingsCubit>()
+                  .state
+                  .settings
+                  .enabledFunctionalities[oldItemIndex];
 
-    return DragAndDropLists(
-      listPadding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-      itemDivider: const Divider(
-        thickness: 2,
-        height: 2,
-        color: Colors.transparent,
-      ),
-      itemDecorationWhileDragging: BoxDecoration(
-        // color: Colors.white,
-        color: Theme.of(context).cardColor,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.5),
-            spreadRadius: 2,
-            blurRadius: 3,
-            offset: const Offset(0, 0), // changes position of shadow
-          ),
-        ],
-      ),
-      itemDragOnLongPress: true,
-      itemDragHandle: DragHandle(
-        child: Padding(
-          padding: const EdgeInsets.only(right: 10),
-          child: Icon(
-            Icons.drag_indicator_rounded,
-            color: Theme.of(context).primaryColor,
-          ),
-        ),
-      ),
-      children: contents,
-      onItemReorder: (int oldItemIndex, int oldListIndex, int newItemIndex,
-          int newListIndex) {
-        if (oldListIndex == 0 && newListIndex == 1) {
-          Functionalities item = context
-              .read<SettingsCubit>()
-              .state
-              .settings
-              .enabledFunctionalities[oldItemIndex];
-          context.read<SettingsCubit>().modify(
-              settings: context.read<SettingsCubit>().state.settings.copyWith(
-                  enabledFunctionalities: context
+              context.read<SettingsCubit>().modify(
+                  settings: context
                       .read<SettingsCubit>()
                       .state
                       .settings
-                      .enabledFunctionalities
-                      .where((element) => element != item)
-                      .toList(),
-                  disabledFunctionalities: context
+                      .copyWith(
+                          enabledFunctionalities: context
+                              .read<SettingsCubit>()
+                              .state
+                              .settings
+                              .enabledFunctionalities
+                              .where((element) => element != item)
+                              .toList(),
+                          disabledFunctionalities: context
+                              .read<SettingsCubit>()
+                              .state
+                              .settings
+                              .disabledFunctionalities
+                              .toList()
+                            ..insert(newItemIndex, item)));
+            } else if (oldListIndex == 1 && newListIndex == 0) {
+              Functionalities item = context
+                  .read<SettingsCubit>()
+                  .state
+                  .settings
+                  .disabledFunctionalities[oldItemIndex];
+              context.read<SettingsCubit>().modify(
+                  settings: context
                       .read<SettingsCubit>()
                       .state
                       .settings
-                      .disabledFunctionalities
-                      .toList()
-                    ..insert(newItemIndex, item)));
-        } else if (oldListIndex == 1 && newListIndex == 0) {
-          Functionalities item = context
-              .read<SettingsCubit>()
-              .state
-              .settings
-              .disabledFunctionalities[oldItemIndex];
-          context.read<SettingsCubit>().modify(
-              settings: context.read<SettingsCubit>().state.settings.copyWith(
-                  disabledFunctionalities: context
+                      .copyWith(
+                          disabledFunctionalities: context
+                              .read<SettingsCubit>()
+                              .state
+                              .settings
+                              .disabledFunctionalities
+                              .where((element) => element != item)
+                              .toList(),
+                          enabledFunctionalities: context
+                              .read<SettingsCubit>()
+                              .state
+                              .settings
+                              .enabledFunctionalities
+                              .toList()
+                            ..insert(newItemIndex, item)));
+            } else if (oldListIndex == 0 && newListIndex == 0) {
+              Functionalities item = context
+                  .read<SettingsCubit>()
+                  .state
+                  .settings
+                  .enabledFunctionalities[oldItemIndex];
+              context.read<SettingsCubit>().modify(
+                  settings: context
                       .read<SettingsCubit>()
                       .state
                       .settings
-                      .disabledFunctionalities
-                      .where((element) => element != item)
-                      .toList(),
-                  enabledFunctionalities: context
+                      .copyWith(
+                          enabledFunctionalities: context
+                              .read<SettingsCubit>()
+                              .state
+                              .settings
+                              .enabledFunctionalities
+                              .toList()
+                            ..removeAt(oldItemIndex)
+                            ..insert(newItemIndex, item)));
+            } else if (oldListIndex == 1 && newListIndex == 1) {
+              Functionalities item = context
+                  .read<SettingsCubit>()
+                  .state
+                  .settings
+                  .disabledFunctionalities[oldItemIndex];
+              context.read<SettingsCubit>().modify(
+                  settings: context
                       .read<SettingsCubit>()
                       .state
                       .settings
-                      .enabledFunctionalities
-                      .toList()
-                    ..insert(newItemIndex, item)));
-        } else if (oldListIndex == 0 && newListIndex == 0) {
-          Functionalities item = context
-              .read<SettingsCubit>()
-              .state
-              .settings
-              .enabledFunctionalities[oldItemIndex];
-          context.read<SettingsCubit>().modify(
-              settings: context.read<SettingsCubit>().state.settings.copyWith(
-                  enabledFunctionalities: context
-                      .read<SettingsCubit>()
-                      .state
-                      .settings
-                      .enabledFunctionalities
-                      .toList()
-                    ..removeAt(oldItemIndex)
-                    ..insert(newItemIndex, item)));
-        } else if (oldListIndex == 1 && newListIndex == 1) {
-          Functionalities item = context
-              .read<SettingsCubit>()
-              .state
-              .settings
-              .disabledFunctionalities[oldItemIndex];
-          context.read<SettingsCubit>().modify(
-              settings: context.read<SettingsCubit>().state.settings.copyWith(
-                  disabledFunctionalities: context
-                      .read<SettingsCubit>()
-                      .state
-                      .settings
-                      .disabledFunctionalities
-                      .toList()
-                    ..removeAt(oldItemIndex)
-                    ..insert(newItemIndex, item)));
-        }
+                      .copyWith(
+                          disabledFunctionalities: context
+                              .read<SettingsCubit>()
+                              .state
+                              .settings
+                              .disabledFunctionalities
+                              .toList()
+                            ..removeAt(oldItemIndex)
+                            ..insert(newItemIndex, item)));
+            }
+          },
+          onListReorder: (int oldListIndex, int newListIndex) {},
+        );
       },
-      onListReorder: (int oldListIndex, int newListIndex) {},
     );
   }
 }
--- lib/screens/settings/widgets/screen_settings_drag_and_drop_item.dart
+++ lib/screens/settings/widgets/screen_settings_drag_and_drop_item.dart
@@ -2,21 +2,65 @@ import 'package:drag_and_drop_lists/drag_and_drop_lists.dart';
 import 'package:flutter/material.dart';
 import 'package:onyx/core/extensions/functionalities_to_human_export.dart';
 import 'package:onyx/core/res.dart';
+import 'package:sizer/sizer.dart';
 
 @override
 DragAndDropItem screenSettingsDragAndDropItem(Functionalities functionality) {
   return DragAndDropItem(
-    child: Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 15,
-        vertical: 10,
-      ),
-      child: Row(
-        children: [
-          Icon(functionality.toIcon()),
-          Text(functionality.toCleanString())
-        ],
-      ),
-    ),
+    child: ScreenSettingsDragAndDropContent(functionality: functionality),
   );
+}
+
+class ScreenSettingsDragAndDropContent extends StatefulWidget {
+  const ScreenSettingsDragAndDropContent(
+      {Key? key, required this.functionality})
+      : super(key: key);
+
+  final Functionalities functionality;
+
+  @override
+  State<ScreenSettingsDragAndDropContent> createState() =>
+      _ScreenSettingsDragAndDropContentState();
+}
+
+class _ScreenSettingsDragAndDropContentState
+    extends State<ScreenSettingsDragAndDropContent> {
+  bool _isExpanded = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 85.w,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Theme.of(context).colorScheme.background,
+      ),
+      child: Theme(
+        data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
+        child: ExpansionTile(
+          onExpansionChanged: (value) {
+            setState(() {
+              _isExpanded = value;
+            });
+          },
+          trailing: const SizedBox(height: 0, width: 0),
+          leading: AnimatedRotation(
+              turns: _isExpanded ? .5 : 0,
+              duration: Res.animationDuration,
+              child: const Icon(
+                  Icons.keyboard_arrow_down_outlined) // your svgImage here
+              ),
+          title: Row(
+            children: [
+              Icon(widget.functionality.toIcon()),
+              Text(widget.functionality.toCleanString())
+            ],
+          ),
+          childrenPadding: const EdgeInsets.all(15),
+          expandedCrossAxisAlignment: CrossAxisAlignment.center,
+          children: [widget.functionality.toSettings()],
+        ),
+      ),
+    );
+  }
 }
