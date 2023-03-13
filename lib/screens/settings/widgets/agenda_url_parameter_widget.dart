08da8122659c8c041c5c7a1e594fb5f4782b9575
Eymeric Dechelette <hatchchien@protonmail.com>
2023-03-13 15:40:06 +0100

    fix agenda config freeze

--- lib/screens/settings/widgets/agenda_url_parameter_widget.dart
+++ lib/screens/settings/widgets/agenda_url_parameter_widget.dart
@@ -1,4 +1,3 @@
-import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:onyx/screens/agenda_config/page/agenda_config_page.dart';
@@ -16,8 +15,7 @@ class AgendaUrlParameterWidget extends StatelessWidget {
       buildWhen: (previous, current) {
         if (previous.settings.fetchAgendaAuto !=
             current.settings.fetchAgendaAuto) {
-          WidgetsBinding.instance
-              .addPostFrameCallback((_) => sizeUpdate());
+          WidgetsBinding.instance.addPostFrameCallback((_) => sizeUpdate());
         }
         return true;
       },
@@ -27,11 +25,7 @@ class AgendaUrlParameterWidget extends StatelessWidget {
             TextSwitchWidget(
               text: 'Récupérer automatiquement les ressources de l\'agenda',
               value:
-              context
-                  .read<SettingsCubit>()
-                  .state
-                  .settings
-                  .fetchAgendaAuto,
+                  context.read<SettingsCubit>().state.settings.fetchAgendaAuto,
               onChanged: (bool b) {
                 context.read<SettingsCubit>().modify(
                     settings: context
@@ -41,53 +35,43 @@ class AgendaUrlParameterWidget extends StatelessWidget {
                         .copyWith(fetchAgendaAuto: b));
               },
             ),
-            if (!context
-                .read<SettingsCubit>()
-                .state
-                .settings
-                .fetchAgendaAuto)
-              Container(
-                clipBehavior: Clip.hardEdge,
-                margin: const EdgeInsets.only(bottom: 15),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                ),
+            if (!context.read<SettingsCubit>().state.settings.fetchAgendaAuto)
+              SizedBox(
+                width: 50.w,
                 child: Center(
-                  child: OpenContainer(
-                    openBuilder: (context, closechild) =>
-                        SafeArea(
-                          child: AgendaConfigPage(
-                            onBack: (index) {
-                              context.read<SettingsCubit>().modify(
-                                  settings: context
-                                      .read<SettingsCubit>()
-                                      .state
-                                      .settings
-                                      .copyWith(agendaId: index));
-                              Navigator.pop(context);
-                            },
-                          ),
-                        ),
-                    closedColor: Theme
-                        .of(context)
-                        .primaryColor,
-                    closedShape: RoundedRectangleBorder(
+                  child: Material(
+                    borderRadius: BorderRadius.circular(100),
+                    color: Theme.of(context).primaryColor,
+                    child: InkWell(
                       borderRadius: BorderRadius.circular(100),
-                    ),
-                    closedBuilder: (context, openchild) =>
-                        InkWell(
-                          onTap: openchild,
-                          child: Container(
-                            padding: EdgeInsets.all(2.5.w),
-                            child: Text(
-                              'Sélectionner l\'agenda',
-                              style: Theme
-                                  .of(context)
-                                  .textTheme
-                                  .bodyLarge,
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => SafeArea(
+                              child: AgendaConfigPage(
+                                onBack: (index) {
+                                  context.read<SettingsCubit>().modify(
+                                      settings: context
+                                          .read<SettingsCubit>()
+                                          .state
+                                          .settings
+                                          .copyWith(agendaId: index));
+                                  Navigator.pop(context);
+                                },
+                              ),
                             ),
                           ),
+                        );
+                      },
+                      child: Container(
+                        padding: EdgeInsets.all(2.5.w),
+                        child: Text(
+                          'Sélectionner l\'agenda',
+                          style: Theme.of(context).textTheme.bodyLarge,
                         ),
+                      ),
+                    ),
                   ),
                 ),
               )
