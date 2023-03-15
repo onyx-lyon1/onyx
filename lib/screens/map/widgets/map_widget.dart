d06abd7e31132214f6dcdc19e22dad4596180cec
Eymeric Dechelette <hatchchien@protonmail.com>
2023-03-15 11:14:36 +0100

    multimodal map
    Former-commit-id: 68772cf034353064d457eb590b871f6bcd818049

--- lib/screens/map/widgets/map_widget.dart
+++ lib/screens/map/widgets/map_widget.dart
@@ -5,16 +5,18 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_marker_popup/flutter_map_marker_popup.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:onyx/core/extensions/extensions_export.dart';
+import 'package:onyx/screens/map/domain/logic/tile_provider_logic.dart';
 import 'package:onyx/screens/map/map_export.dart';
 import 'package:sizer/sizer.dart';
 
 class MapWidget extends StatelessWidget {
-  const MapWidget({Key? key,
-    this.batiments = const [],
-    this.polylines = const [],
-    required this.onTapNavigate,
-    this.mapController,
-    this.center})
+  const MapWidget(
+      {Key? key,
+      this.batiments = const [],
+      this.polylines = const [],
+      required this.onTapNavigate,
+      this.mapController,
+      this.center})
       : super(key: key);
   final List<BatimentModel> batiments;
   final List<Polyline> polylines;
@@ -29,12 +31,11 @@ class MapWidget extends StatelessWidget {
     List<Marker> markers = batiments.map((element) {
       return Marker(
         point: element.position,
-        builder: (context) =>
-            Icon(
-              Icons.location_on_rounded,
-              size: 15.sp,
-              color: Colors.red,
-            ),
+        builder: (context) => Icon(
+          Icons.location_on_rounded,
+          size: 15.sp,
+          color: Colors.red,
+        ),
       );
     }).toList();
     if (center == null) {
@@ -48,20 +49,11 @@ class MapWidget extends StatelessWidget {
       options: MapOptions(
         center: center ?? MapRes.center,
         zoom: 16.5,
-        maxZoom: MapRes.maxZoom,
-        minZoom: MapRes.minZoom,
-        maxBounds: LatLngBounds(
-          MapRes.minBound,
-          MapRes.maxBound,
-        ),
       ),
       mapController: mapController,
       children: [
-        TileLayer(
-          urlTemplate: "assets/map/{z}/{x}/{y}.png",
-          userAgentPackageName: 'dev.fleaflet.flutter_map.example',
-          tileProvider: AssetTileProvider(),
-        ),
+        TileLayer(tileProvider: HybridTileProvider() //AssetTileProvider(),
+            ),
         if (polylines.isNotEmpty) PolylineLayer(polylines: polylines),
         if (!Platform.isLinux && !Platform.isMacOS && !Platform.isWindows)
           const CustomCurrentLocationLayerWidget(),
@@ -73,7 +65,7 @@ class MapWidget extends StatelessWidget {
               popupBuilder: (BuildContext context, Marker marker) {
                 return MapPopupWidget(
                   batiment: batiments.firstWhere(
-                          (element) => element.position == marker.point),
+                      (element) => element.position == marker.point),
                   onTap: onTapNavigate,
                   popupController: popupLayerController,
                 );
