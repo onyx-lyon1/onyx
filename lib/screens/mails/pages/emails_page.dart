b3f5860ecec3fea2a94426b5e9099acbf98cb76b
Eymeric Dechelette <hatchchien@protonmail.com>
2023-03-29 12:58:11 +0200

    some small ui improovment on emails
    Former-commit-id: 40b848a0e908052e22f8aa8d5d13b6fbd1fa2227

--- lib/screens/mails/pages/emails_page.dart
+++ lib/screens/mails/pages/emails_page.dart
@@ -1,6 +1,8 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:onyx/core/res.dart';
 import 'package:onyx/core/widgets/core_widget_export.dart';
 import 'package:onyx/screens/login/login_export.dart';
 import 'package:onyx/screens/mails/mails_export.dart';
@@ -30,59 +32,70 @@ class EmailsPage extends StatelessWidget {
             state.status == EmailStatus.cacheSorted ||
             state.status == EmailStatus.mailboxesLoaded) {
           loadingHeader =
-              const LoadingHeaderWidget(message: "Chargement des emails");
+          const LoadingHeaderWidget(message: "Chargement des emails");
         } else if (state.status == EmailStatus.error) {
           loadingHeader = const LoadingHeaderWidget(
             message: "Erreur de chargement des emails",
           );
         } else if (state.status == EmailStatus.initial) {
           context.read<EmailCubit>().connect(
-              username: context.read<AuthentificationCubit>().state.username,
-              password: context.read<AuthentificationCubit>().state.password);
+              username: context
+                  .read<AuthentificationCubit>()
+                  .state
+                  .username,
+              password: context
+                  .read<AuthentificationCubit>()
+                  .state
+                  .password);
         } else if (state.status == EmailStatus.connected) {
           context.read<EmailCubit>().load(
-                blockTrackers:
-                    context.read<SettingsCubit>().state.settings.blockTrackers,
-              );
+            blockTrackers:
+            context
+                .read<SettingsCubit>()
+                .state
+                .settings
+                .blockTrackers,
+          );
         }
         return Scaffold(
-          backgroundColor: Theme.of(context).colorScheme.background,
-          floatingActionButton: IconButton(
-            onPressed: () {
-              context.read<EmailCubit>().load(
-                    blockTrackers: context
-                        .read<SettingsCubit>()
-                        .state
-                        .settings
-                        .blockTrackers,
-                  );
-            },
-            icon: const Icon(Icons.refresh),
+          backgroundColor: Theme
+              .of(context)
+              .colorScheme
+              .background,
+          floatingActionButton:
+          OpenContainer(
+            openColor: Theme
+                .of(context)
+                .colorScheme
+                .background,
+            closedColor: (!state.connected)
+                ? Theme
+                .of(context)
+                .disabledColor
+                : Theme
+                .of(context)
+                .primaryColor,
+            closedShape: const CircleBorder(),
+            closedElevation: 6,
+            transitionDuration: Res.animationDuration,
+            tappable: state.connected,
+            openBuilder: (context, closedContainer) => const EmailSendPage(),
+            closedBuilder: (context, openContainer) =>
+                InkWell(
+                  onTap: (state.connected) ? openContainer : null,
+                  child: Padding(
+                    padding: EdgeInsets.all(1.5.h),
+                    child: Icon(
+                      Icons.create_rounded,
+                      color: Theme
+                          .of(context)
+                          .bottomNavigationBarTheme
+                          .unselectedItemColor,
+                      size: 25.sp,
+                    ),
+                  ),
+                ),
           ),
-          // OpenContainer(
-          //   openColor: Theme.of(context).colorScheme.background,
-          //   closedColor: (!state.connected)
-          //       ? Theme.of(context).disabledColor
-          //       : Theme.of(context).primaryColor,
-          //   closedShape: const CircleBorder(),
-          //   closedElevation: 6,
-          //   transitionDuration: Res.animationDuration,
-          //   tappable: state.connected,
-          //   openBuilder: (context, closedContainer) => const EmailSendPage(),
-          //   closedBuilder: (context, openContainer) => InkWell(
-          //     onTap: (state.connected) ? openContainer : null,
-          //     child: Padding(
-          //       padding: EdgeInsets.all(1.5.h),
-          //       child: Icon(
-          //         Icons.create_rounded,
-          //         color: Theme.of(context)
-          //             .bottomNavigationBarTheme
-          //             .unselectedItemColor,
-          //         size: 25.sp,
-          //       ),
-          //     ),
-          //   ),
-          // ),
           body: CommonScreenWidget(
             state: loadingHeader,
             header: const EmailHeaderWidget(),
@@ -94,41 +107,46 @@ class EmailsPage extends StatelessWidget {
                   child: ListView.custom(
                     controller: scrollController,
                     childrenDelegate:
-                        SliverChildBuilderDelegate((context, index) {
+                    SliverChildBuilderDelegate((context, index) {
                       if (index < state.currentMailBox!.emails.length) {
                         return EmailWidget(
                             email: state.currentMailBox!.emails[index]);
                       } else if ((index ==
-                              state.currentMailBox!.emails.length) &&
+                          state.currentMailBox!.emails.length) &&
                           state.currentMailBox!.emails.isNotEmpty) {
                         return Material(
-                          color: Theme.of(context).colorScheme.background,
+                          color: Theme
+                              .of(context)
+                              .colorScheme
+                              .background,
                           child: (state.status == EmailStatus.loading)
                               ? Center(
-                                  child: Padding(
-                                    padding: EdgeInsets.all(5.w),
-                                    child: CircularProgressIndicator(
-                                      color: Theme.of(context).primaryColor,
-                                    ),
-                                  ),
-                                )
+                            child: Padding(
+                              padding: EdgeInsets.all(5.w),
+                              child: CircularProgressIndicator(
+                                color: Theme
+                                    .of(context)
+                                    .primaryColor,
+                              ),
+                            ),
+                          )
                               : InkWell(
-                                  onTap: () =>
-                                      context.read<EmailCubit>().increaseNumber(
-                                            blockTrackers: context
-                                                .read<SettingsCubit>()
-                                                .state
-                                                .settings
-                                                .blockTrackers,
-                                          ),
-                                  child: Center(
-                                    child: Padding(
-                                      padding: EdgeInsets.all(8.w),
-                                      child: const Text(
-                                          "Charger 20 messages de plus"),
-                                    ),
-                                  ),
+                            onTap: () =>
+                                context.read<EmailCubit>().increaseNumber(
+                                  blockTrackers: context
+                                      .read<SettingsCubit>()
+                                      .state
+                                      .settings
+                                      .blockTrackers,
                                 ),
+                            child: Center(
+                              child: Padding(
+                                padding: EdgeInsets.all(8.w),
+                                child: const Text(
+                                    "Charger 20 messages de plus"),
+                              ),
+                            ),
+                          ),
                         );
                       }
                       return null;
@@ -140,12 +158,12 @@ class EmailsPage extends StatelessWidget {
             ),
             onRefresh: () async {
               context.read<EmailCubit>().load(
-                    blockTrackers: context
-                        .read<SettingsCubit>()
-                        .state
-                        .settings
-                        .blockTrackers,
-                  );
+                blockTrackers: context
+                    .read<SettingsCubit>()
+                    .state
+                    .settings
+                    .blockTrackers,
+              );
               while (state.status != EmailStatus.loaded &&
                   state.status != EmailStatus.error &&
                   state.status != EmailStatus.sorted) {
