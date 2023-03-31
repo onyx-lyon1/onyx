c169bfbc8cee9c0379a03377a69ba89a7db08ea4
Eymeric Dechelette <hatchchien@protonmail.com>
2023-04-01 00:23:10 +0200

    set dekstop window size on startup and remove some prints

--- linux/flutter/generated_plugin_registrant.cc
+++ linux/flutter/generated_plugin_registrant.cc
@@ -6,10 +6,14 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <desktop_window/desktop_window_plugin.h>
 #include <pasteboard/pasteboard_plugin.h>
 #include <url_launcher_linux/url_launcher_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) desktop_window_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "DesktopWindowPlugin");
+  desktop_window_plugin_register_with_registrar(desktop_window_registrar);
   g_autoptr(FlPluginRegistrar) pasteboard_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "PasteboardPlugin");
   pasteboard_plugin_register_with_registrar(pasteboard_registrar);
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -4,6 +4,7 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   connectivity_plus
+  desktop_window
   geolocator_windows
   pasteboard
   permission_handler_windows
