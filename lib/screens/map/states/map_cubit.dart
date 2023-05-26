16473f4bf7bfcdd15de70902bce9da94b0d8c785
Eymeric Dechelette <hatchchien@protonmail.com>
2023-05-26 20:24:01 +0200

    map local routing

--- lib/screens/map/states/map_cubit.dart
+++ lib/screens/map/states/map_cubit.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:onyx/screens/map/map_export.dart';
 
@@ -10,11 +9,7 @@ part 'map_state.dart';
 class MapCubit extends Cubit<MapState> {
   MapCubit() : super(MapState());
 
-  void navigate(BuildContext context, BatimentModel batiment,
-      MapController? mapController) async {
-    if (mapController != null) {
-      mapController.move(batiment.position, 18.0);
-    }
+  Future<void> navigate(BuildContext context, BatimentModel batiment) async {
     List<List<LatLng>> paths =
         await NavigationLogic.navigateToBatimentFromLocation(
       context,
