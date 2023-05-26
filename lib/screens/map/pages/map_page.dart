bc9d85e5dba274beb51e6abd8515af6f5c0072f4
Eymeric Dechelette <hatchchien@protonmail.com>
2023-05-26 20:24:01 +0200

    map local routing
    Former-commit-id: bd66f5edc3923b789b89b50514520965c2457aeb

--- lib/screens/map/pages/map_page.dart
+++ lib/screens/map/pages/map_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 import 'package:onyx/core/widgets/core_widget_export.dart';
 import 'package:onyx/screens/map/map_export.dart';
 
@@ -10,22 +11,25 @@ class MapPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final textEditingController = TextEditingController();
-    final MapController mapController = MapController();
     return BlocBuilder<MapCubit, MapState>(
       builder: (context, state) {
+        LatLng? center;
         if (state.status == MapStatus.initial) {
           context.read<MapCubit>().loadBatiment();
+        }
+        if (state.status == MapStatus.batimentsUpdated &&
+            state.path.isNotEmpty) {
+          center = state.path.first;
         }
         return CommonScreenWidget(
           header: Center(
             child: MapSearchAutocompleteWidget(
               controller: textEditingController,
-              mapController: mapController,
             ),
           ),
           body: MapWidget(
             batiments: state.batiments,
-            mapController: mapController,
+            center: center,
             polylines: [
               Polyline(
                 points: state.path,
@@ -34,9 +38,7 @@ class MapPage extends StatelessWidget {
               ),
             ],
             onTapNavigate: (batiment) {
-              context
-                  .read<MapCubit>()
-                  .navigate(context, batiment, mapController);
+              context.read<MapCubit>().navigate(context, batiment);
             },
           ),
         );
--- lib/screens/map/widgets/map_widget.dart
+++ lib/screens/map/widgets/map_widget.dart
@@ -1,10 +1,10 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_marker_popup/flutter_map_marker_popup.dart';
 import 'package:latlong2/latlong.dart';
-import 'package:onyx/core/extensions/extensions_export.dart';
 import 'package:onyx/screens/map/domain/logic/tile_provider_logic.dart';
 import 'package:onyx/screens/map/map_export.dart';
 import 'package:sizer/sizer.dart';
@@ -29,7 +29,7 @@ class MapWidget extends StatefulWidget {
 }
 
 class _MapWidgetState extends State<MapWidget> {
-  late MapController? mapController;
+  late MapController mapController;
 
   @override
   void initState() {
@@ -54,100 +54,112 @@ class _MapWidgetState extends State<MapWidget> {
     if (widget.center == null) {
       GeolocationLogic.getCurrentLocation(askPermission: false)
           .then((value) async {
-        if ((value != null) && value.inside(MapRes.minBound, MapRes.maxBound)) {
-          //loop 10 times to make sure the map controller is initialized
-          for (int i = 0; i < 10; i++) {
-            if (mapController != null) {
-              mapController?.move(value, 16.5);
-              break;
-            }
-            await Future.delayed(const Duration(milliseconds: 100));
-          }
+        if (value != null) {
+          print("lets move to the user positions");
+          mapController.move(value, 16.5);
         }
       });
     }
-    return Stack(
-      children: [
-        FlutterMap(
-          options: MapOptions(
-            center: widget.center ?? MapRes.center,
-            zoom: 16.5,
-            maxZoom: MapRes.maxZoom,
-            minZoom: 0,
-          ),
-          mapController: mapController,
-          children: [
-            TileLayer(tileProvider: HybridTileProvider() //AssetTileProvider(),
-                ),
-            if (widget.polylines.isNotEmpty)
-              PolylineLayer(polylines: widget.polylines),
-            if (!Platform.isLinux && !Platform.isMacOS && !Platform.isWindows)
-              const CustomCurrentLocationLayerWidget(),
-            if (widget.batiments.isNotEmpty)
-              PopupMarkerLayerWidget(
-                options: PopupMarkerLayerOptions(
-                  markers: markers,
-                  popupController: popupLayerController,
-                  popupBuilder: (BuildContext context, Marker marker) {
-                    return MapPopupWidget(
-                      batiment: widget.batiments.firstWhere(
-                          (element) => element.position == marker.point),
-                      onTap: widget.onTapNavigate,
-                      popupController: popupLayerController,
-                    );
-                  },
-                ),
-              ),
-          ],
-        ),
-        Align(
-          alignment: Alignment.bottomRight,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            mainAxisAlignment: MainAxisAlignment.end,
+    return BlocListener<MapCubit, MapState>(
+      listener: (context, state) {
+        print("state changed");
+        print(state.status);
+        print("path length: ${state.path.length}");
+        if (state.status == MapStatus.batimentsUpdated &&
+            state.path.isNotEmpty) {
+          print("lets move to the first point");
+          mapController.move(state.path.first, 18.0);
+        }
+      },
+      child: Stack(
+        children: [
+          FlutterMap(
+            options: MapOptions(
+              center: widget.center ?? MapRes.center,
+              zoom: 16.5,
+              maxZoom: MapRes.maxZoom,
+              minZoom: 0,
+            ),
+            mapController: mapController,
             children: [
+              TileLayer(
+                  tileProvider: HybridTileProvider() //AssetTileProvider(),
+                  ),
+              if (widget.polylines.isNotEmpty)
+                PolylineLayer(polylines: widget.polylines),
               if (!Platform.isLinux && !Platform.isMacOS && !Platform.isWindows)
+                const CustomCurrentLocationLayerWidget(),
+              if (widget.batiments.isNotEmpty)
+                PopupMarkerLayerWidget(
+                  options: PopupMarkerLayerOptions(
+                    markers: markers,
+                    popupController: popupLayerController,
+                    popupBuilder: (BuildContext context, Marker marker) {
+                      return MapPopupWidget(
+                        batiment: widget.batiments.firstWhere(
+                            (element) => element.position == marker.point),
+                        onTap: widget.onTapNavigate,
+                        popupController: popupLayerController,
+                      );
+                    },
+                  ),
+                ),
+            ],
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                if (!Platform.isLinux &&
+                    !Platform.isMacOS &&
+                    !Platform.isWindows)
+                  Padding(
+                    padding: EdgeInsets.all(2.h),
+                    child: IconButton(
+                        onPressed: () {
+                          GeolocationLogic.getCurrentLocation(
+                                  askPermission: true)
+                              .then((value) {
+                            setState(() {
+                              if ((value != null)) {
+                                print("move due to button to location");
+                                mapController.move(value, 15);
+                              }
+                            });
+                          });
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                              Theme.of(context).primaryColor),
+                        ),
+                        icon: Icon(
+                          Icons.location_searching_rounded,
+                          size: 25.sp,
+                        )),
+                  ),
                 Padding(
                   padding: EdgeInsets.all(2.h),
                   child: IconButton(
                       onPressed: () {
-                        GeolocationLogic.getCurrentLocation(askPermission: true)
-                            .then((value) {
-                          setState(() {
-                            if ((value != null)) {
-                              mapController?.move(value, 15);
-                            }
-                          });
-                        });
+                        print("move to campus");
+                        mapController.move(MapRes.center, 16.5);
                       },
                       style: ButtonStyle(
                         backgroundColor: MaterialStateProperty.all(
                             Theme.of(context).primaryColor),
                       ),
                       icon: Icon(
-                        Icons.location_searching_rounded,
+                        Icons.location_city_rounded,
                         size: 25.sp,
                       )),
                 ),
-              Padding(
-                padding: EdgeInsets.all(2.h),
-                child: IconButton(
-                    onPressed: () {
-                      mapController!.move(MapRes.center, 16.5);
-                    },
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(
-                          Theme.of(context).primaryColor),
-                    ),
-                    icon: Icon(
-                      Icons.location_city_rounded,
-                      size: 25.sp,
-                    )),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
