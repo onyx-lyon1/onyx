0b68fcc5f768cd90fbf6b9b75c5d7d9dbb502f11
eymeric <eymericdechelette@gmail.com>
2023-06-13 19:21:33 +0200

    speedup
    Former-commit-id: d76d444aa421d4ea219a01fbe15b593648b87a20

--- lib/screens/tomuss/widgets/enumeration_widget.dart
+++ lib/screens/tomuss/widgets/enumeration_widget.dart
@@ -12,107 +12,61 @@ class EnumerationWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<TomussCubit, TomussState>(
-      builder: (context, state) {
-        print("building");
-        if (this.enumeration.modifiable) {
-          print(context
-              .read<TomussCubit>()
-              .state
-              .teachingUnits
-              .firstWhere((element) =>
-                  element.enumerations.isNotEmpty &&
-                  element.enumerations
-                          .indexWhere((element2) => element2.modifiable) !=
-                      -1)
-              .enumerations
-              .where((element) => element.modifiable)
-              .map((e) => e.value));
-        }
-
-        Enumeration? enumeration;
-        for (var i in state.teachingUnits) {
-          for (var j in i.enumerations) {
-            if (j == this.enumeration) {
-              enumeration = j;
-            }
-          }
-        }
-        enumeration ??= this.enumeration;
-        if (this.enumeration.modifiable || enumeration.modifiable) {
-          print("!!!!!!!!!!!!!!!!!${this.enumeration.value}");
-          print("!!!!!!!!!!!!!!!!!${enumeration.value}");
-        }
-        return TomussElementWidget(
-          left: (enumeration.modifiable)
-              ? PopupMenuButton(
-                  color: Theme.of(context).colorScheme.background,
-                  initialValue: enumeration.value,
-                  itemBuilder: (BuildContext context) {
-                    return enumeration!.values.map((String value) {
-                      return PopupMenuItem<String>(
-                        value: value,
-                        child: Text(
-                          value,
-                          maxLines: 3,
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Theme.of(context).textTheme.bodyLarge!.color,
-                            overflow: TextOverflow.ellipsis,
-                            fontSize: 11.sp,
-                          ),
-                        ),
-                      );
-                    }).toList();
-                  },
-                  onSelected: (String value) {
-                    print("!!!!!!!!!!!!!!!!!Selected $value");
-                    context
-                        .read<TomussCubit>()
-                        .updateEnumerationValue(enumeration!, value);
-                  },
-                  child: Row(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Flexible(
-                        child: Text(
-                          enumeration.value ?? "",
-                          maxLines: 4,
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Theme.of(context).textTheme.bodyLarge!.color,
-                            overflow: TextOverflow.ellipsis,
-                            fontSize: 11.sp,
-                          ),
-                        ),
+    return TomussElementWidget(
+      left: (enumeration.modifiable)
+          ? PopupMenuButton(
+              color: Theme.of(context).colorScheme.background,
+              initialValue: enumeration.value,
+              itemBuilder: (BuildContext context) {
+                return enumeration.values.map((String value) {
+                  return PopupMenuItem<String>(
+                    value: value,
+                    child: Text(
+                      value,
+                      maxLines: 3,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).textTheme.bodyLarge!.color,
+                        overflow: TextOverflow.ellipsis,
+                        fontSize: 11.sp,
                       ),
-                      Icon(
-                        Icons.arrow_drop_down,
-                        size: 11.sp,
+                    ),
+                  );
+                }).toList();
+              },
+              onSelected: (String value) {
+                context
+                    .read<TomussCubit>()
+                    .updateEnumerationValue(enumeration, value);
+              },
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Flexible(
+                    child: Text(
+                      enumeration.value ?? "",
+                      maxLines: 4,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).textTheme.bodyLarge!.color,
+                        overflow: TextOverflow.ellipsis,
+                        fontSize: 11.sp,
                       ),
-                    ],
-                  ))
-              : Center(
-                  child: Text(
-                    enumeration.values.last,
-                    maxLines: 4,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Theme.of(context).textTheme.bodyLarge!.color,
-                      overflow: TextOverflow.ellipsis,
-                      fontSize: 11.sp,
                     ),
                   ),
-                ),
-          right: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                enumeration.title,
-                maxLines: 1,
+                  Icon(
+                    Icons.arrow_drop_down,
+                    size: 11.sp,
+                  ),
+                ],
+              ))
+          : Center(
+              child: Text(
+                enumeration.values.last,
+                maxLines: 4,
+                textAlign: TextAlign.center,
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
                   color: Theme.of(context).textTheme.bodyLarge!.color,
@@ -120,19 +74,31 @@ class EnumerationWidget extends StatelessWidget {
                   fontSize: 11.sp,
                 ),
               ),
-              Text(
-                "${enumeration.author}\n${enumeration.comment}",
-                maxLines: 3,
-                style: TextStyle(
-                  color: Theme.of(context).textTheme.bodyLarge!.color,
-                  overflow: TextOverflow.ellipsis,
-                  fontSize: 7.sp,
-                ),
-              ),
-            ],
+            ),
+      right: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            enumeration.title,
+            maxLines: 1,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Theme.of(context).textTheme.bodyLarge!.color,
+              overflow: TextOverflow.ellipsis,
+              fontSize: 11.sp,
+            ),
           ),
-        );
-      },
+          Text(
+            "${enumeration.author}\n${enumeration.comment}",
+            maxLines: 3,
+            style: TextStyle(
+              color: Theme.of(context).textTheme.bodyLarge!.color,
+              overflow: TextOverflow.ellipsis,
+              fontSize: 7.sp,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
