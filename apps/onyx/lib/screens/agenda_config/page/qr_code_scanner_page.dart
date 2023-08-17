cf7283df9b32807e0e04effb4b276efd6fdd9c7d
eymeric <eymericdechelette@gmail.com>
2023-08-17 12:43:01 +0200

    je fait des betises
    Former-commit-id: d24fe1ef6d109ffc4971c0b75593b4d8f63c41d6

--- apps/onyx/lib/screens/agenda_config/page/qr_code_scanner_page.dart
+++ apps/onyx/lib/screens/agenda_config/page/qr_code_scanner_page.dart
@@ -34,16 +34,23 @@ class QrCodeScannerPageState extends State<QrCodeScannerPage> {
       body: Container(
         color: Theme.of(context).colorScheme.background,
         child: cameraGranted
-            ? MobileScanner(
-                controller: MobileScannerController(
-                  detectionSpeed: DetectionSpeed.noDuplicates,
-                ),
-                onDetect: (barcode) {
-                  final String code = barcode.barcodes.first.rawValue ?? '';
-                  if (code.isNotEmpty && code.startsWith('http')) {
-                    Navigator.pop(context, code);
-                  }
-                })
+            ? MobileScanner(onDetect: (barcode, args) {
+                final String code = barcode.rawValue ?? '';
+                if (code.isNotEmpty && code.startsWith('http')) {
+                  Navigator.pop(context, code);
+                }
+              })
+
+            // MobileScanner(
+            //         controller: MobileScannerController(
+            //           detectionSpeed: DetectionSpeed.noDuplicates,
+            //         ),
+            //         onDetect: (barcode) {
+            //           final String code = barcode.barcodes.first.rawValue ?? '';
+            //           if (code.isNotEmpty && code.startsWith('http')) {
+            //             Navigator.pop(context, code);
+            //           }
+            //         })
             : const Center(
                 child: Text(
                   /* TODO: beautify this screen !*/
