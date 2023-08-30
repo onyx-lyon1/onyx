bb472c0f6009caec13d14df6491036b9ec022eb4
eymeric <eymericdechelette@gmail.com>
2023-08-30 21:28:54 +0200

    handle better when ids failes
    Former-commit-id: 4b357ee1a30604f068a3aeeab5a1b2bde88eba9d

--- packages/lyon1agendaclient/lib/lyon1agendaclient.dart
+++ packages/lyon1agendaclient/lib/lyon1agendaclient.dart
@@ -2,3 +2,4 @@ export 'src/lyon1agendaclient.dart' show Lyon1AgendaClient;
 export 'src/model/agenda.dart' show Agenda;
 export 'src/model/day.dart';
 export 'src/model/event.dart';
+export 'src/utils/agenda_url.dart' show AutoIdException;
