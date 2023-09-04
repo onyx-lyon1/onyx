76c0f1bdc1a1db20efd71dd10520ddec14fb2c32
eymeric <eymericdechelette@gmail.com>
2023-09-04 23:36:04 +0200

    menu v1

--- apps/onyx/lib/core/search/search_service.dart
+++ apps/onyx/lib/core/search/search_service.dart
@@ -1,21 +1,37 @@
 class SearchService {
   static bool isMatch(String a, String b) {
-    final stopWords = ["le", "la", "un", "une", "des", "d'un", "d'une", "en"];
+    final stopWords = [
+      "le",
+      "la",
+      "un",
+      "une",
+      "des",
+      "d'un",
+      "d'une",
+      "en",
+      "a",
+      "de",
+      "cafeteria",
+      "restaurant"
+    ];
 
-    List<String?> aTerms = a.toLowerCase().split(" ");
-    aTerms = aTerms.where((term) => !stopWords.contains(term)).toList();
-    aTerms = aTerms.where((term) => term!.length > 1).toList();
+    List<String> aTerms = a.toLowerCase().split(" ");
+    aTerms = aTerms.where((term) => term.length > 1).toList();
     aTerms = aTerms
-        .map((e) => e!
+        .map((e) => e
             .replaceAll(RegExp(r"[àáâãäå]"), 'a')
             .replaceAll(RegExp(r"[éèêë]"), 'e')
             .replaceAll(RegExp(r"[îï]"), 'i')
             .replaceAll(RegExp(r"[ôö]"), 'o')
             .replaceAll(RegExp(r"[ùúûü]"), 'u'))
         .toList();
+
     //remove special characters
-    aTerms = aTerms.map((e) => e!.replaceAll(RegExp(r"[^\w\s]"), '')).toList();
-    aTerms = aTerms.where((term) => !term!.contains("amphi")).toList();
+    aTerms = aTerms.map((e) => e.replaceAll(RegExp(r"[^\w\s]"), '')).toList();
+    aTerms = aTerms.where((term) => !term.contains("amphi")).toList();
+
+    //remove stop words
+    aTerms = aTerms.where((term) => !stopWords.contains(term)).toList();
 
     b = b.toLowerCase();
     b = b
@@ -28,7 +44,7 @@ class SearchService {
     b = b.replaceAll(RegExp(r"[^\w\s]"), '');
 
     for (var term in aTerms) {
-      if (b.contains(term!)) {
+      if (b.contains(term)) {
         return true;
       }
     }
--- apps/onyx/lib/screens/map/states/map_cubit.dart
+++ apps/onyx/lib/screens/map/states/map_cubit.dart
@@ -27,20 +27,23 @@ class MapCubit extends Cubit<MapState> {
 
   void loadBatiment() async {
     emit(state.copyWith(status: MapStatus.loading));
-    if (BatimentsLogic.batiments.isEmpty) {
-      await BatimentsLogic.loadBatiments();
+    List<BatimentModel> batiments = [];
+    if (state.batiments.isEmpty) {
+      batiments = await BatimentsLogic.loadBatiments();
     }
-    emit(state.copyWith(batiments: BatimentsLogic.batiments));
+    emit(state.copyWith(batiments: batiments));
     List<RestaurantModel> restaurant;
-    if (await CacheService.exist<List<RestaurantModel>>()) {
-      restaurant = (await CacheService.get<List<RestaurantModel>>())!;
+    if (await CacheService.exist<RestaurantListModel>()) {
+      restaurant =
+          (await CacheService.get<RestaurantListModel>())!.restaurantList;
       emit(state.copyWith(
           restaurant: restaurant, status: MapStatus.batimentsUpdated));
     }
     restaurant = await IzlyClient.getRestaurantCrous();
     emit(state.copyWith(
         restaurant: restaurant, status: MapStatus.batimentsUpdated));
-    await CacheService.set<List<RestaurantModel>>(restaurant);
+    await CacheService.set<RestaurantListModel>(
+        RestaurantListModel(restaurantList: restaurant));
   }
 
   void resetCubit() {
