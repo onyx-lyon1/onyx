b5ee27e049757b0101a27b19a1ae1678a63b6340
eymeric <eymericdechelette@gmail.com>
2023-09-26 22:44:20 +0200

    indicate weekday number and name

---
+++ apps/onyx/lib/screens/agenda/widgets/multiple_day_view/multiple_day_view_res.dart
@@ -0,0 +1,8 @@
+import 'package:responsive_sizer/responsive_sizer.dart';
+
+class MultipleDayViewRes {
+  static const double heightFactor = 3.0;
+  static const double leftHourIndicatorWidth = 10.0;
+
+  static double columnWidth = 0.0;
+}
