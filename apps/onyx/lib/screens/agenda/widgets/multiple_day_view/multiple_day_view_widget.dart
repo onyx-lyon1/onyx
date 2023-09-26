3a02791fed2d33954256c15269eb6b0180a5ca2c
eymeric <eymericdechelette@gmail.com>
2023-09-26 20:13:59 +0200

    some untested big cleaning

---
+++ apps/onyx/lib/screens/agenda/widgets/multiple_day_view/multiple_day_view_widget.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lyon1agendaclient/lyon1agendaclient.dart';
+import 'package:onyx/core/res.dart';
+import 'package:onyx/screens/agenda/agenda_export.dart';
+import 'package:onyx/screens/settings/settings_export.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+
+class MultipleDayViewWidget extends StatelessWidget {
+  final PageController pageController;
+
+  const MultipleDayViewWidget({
+    Key? key,
+    required this.pageController,
+  }) : super(key: key);
+
+  static const double heightFactor = 3.0;
+  static const double leftHourIndicatorWidth = 8.0;
+
+  @override
+  Widget build(BuildContext context) {
+    final double columnWidth = (100 - leftHourIndicatorWidth).w /
+        context.read<AgendaCubit>().state.dayCount;
+    return PageView(
+      controller: pageController,
+      scrollDirection: Axis.vertical,
+      key: UniqueKey(),
+      // pas le plus propre mais force a rebuild et reinit le controller
+      onPageChanged: (index) {
+        final AgendaState agendaState = context.read<AgendaCubit>().state;
+        if (context.read<SettingsCubit>().state.settings.showMiniCalendar &&
+            !agendaState.animating) {
+          if (agendaState.days.length > index) {
+            context.read<AgendaCubit>().updateDisplayedDate(
+                date: agendaState.days[index].date, fromPageController: true);
+          }
+        }
+      },
+      children: [
+        for (int i = 0;
+            i < context.read<AgendaCubit>().state.days.length;
+            i = i + context.read<AgendaCubit>().state.dayCount)
+          SingleChildScrollView(
+            child: Row(
+              children: [
+                //left hour indicator
+                const LeftHourIndicatorWidget(
+                    heightFactor: heightFactor,
+                    leftHourIndicatorWidth: leftHourIndicatorWidth),
+                for (int j = 0;
+                    j < context.read<AgendaCubit>().state.dayCount &&
+                        i + j < context.read<AgendaCubit>().state.days.length;
+                    j++)
+                  Container(
+                    color: Colors.red,
+                    width: columnWidth,
+                    height: (Res.agendaDayDuration.inHours / heightFactor).h *
+                        (Res.agendaDayDuration.inHours - 1),
+                    child: Column(
+                      children: buildEventWidgetList(
+                        context.read<AgendaCubit>().state.days[i + j].events,
+                        columnWidth,
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+          ),
+      ],
+    );
+  }
+
+  List<Widget> buildEventWidgetList(List<Event> events, double columnWidth) {
+    List<Widget> result = [];
+    Map<int, List<int>> superposition = {};
+
+    //construct superposition map
+    for (int index = 0; index < events.length - 1; index++) {
+      if (events[index].end.isAfter(events[index + 1].start)) {
+        //there is one superposition
+        bool found = false;
+        for (var sup in superposition.keys) {
+          if (superposition[sup]!.contains(index)) {
+            superposition[sup]!.add(index + 1);
+            found = true;
+          }
+        }
+        if (!found) {
+          superposition[index] = [index, index + 1];
+        }
+      }
+    }
+
+    for (int index = 0; index < events.length - 1; index++) {
+      double diff = 0.0;
+      Map<int, double> diffMap = {};
+      if (superposition.keys.contains(index)) {
+        for (int j = 0; j < superposition[index]!.length; j++) {
+          diffMap[j] = getDiff(
+              events[superposition[index]![j]],
+              //if we have to compare to the previous event in the calendar
+              (j == 0)
+                  //if there is a previous one
+                  ? ((index > 0) ? events[index - 1] : null)
+                  : events[superposition[index]![j - 1]]);
+        }
+        diff = diffMap[superposition[index]!.first]!;
+
+        //-1 because the for loop will add it
+        index += superposition[index]!.length - 1;
+      } else {
+        diff = getDiff(events[index], events.elementAtOrNull(index - 1));
+      }
+      result.add(
+        Padding(
+          padding: EdgeInsets.only(
+            top: (diff * (Res.agendaDayDuration.inHours / heightFactor).h -
+                    0.5.h)
+                .clamp(0, double.infinity),
+          ),
+          child: (superposition.keys.contains(index))
+              ? Row(
+                  children: [
+                    for (var i in superposition[index]!)
+                      Padding(
+                        padding: EdgeInsets.only(
+                          top: (superposition[index]!.first != i)
+                              ? (diffMap[i]! *
+                                          (Res.agendaDayDuration.inHours /
+                                                  heightFactor)
+                                              .h -
+                                      0.5.h)
+                                  .clamp(0, double.infinity)
+                              : 0.0,
+                        ),
+                        child: SizedEventWidget(
+                            heightFactor: heightFactor,
+                            numberPerColumn:
+                                (superposition[index]?.length) ?? 1,
+                            columnWidth: columnWidth,
+                            event: events[i]),
+                      ),
+                  ],
+                )
+              : SizedEventWidget(
+                  heightFactor: heightFactor,
+                  numberPerColumn: 1,
+                  columnWidth: columnWidth,
+                  event: events[index]),
+        ),
+      );
+    }
+
+    return result;
+  }
+
+  double getDiff(Event a, Event? b) {
+    if (b != null) {
+      return a.start.difference(b.end).inMinutes / 60;
+    } else {
+      return a.start
+              .difference(DateTime(a.start.year, a.start.month, a.start.day,
+                  Res.agendaDayStart.inHours))
+              .inMinutes /
+          60;
+    }
+  }
+}
