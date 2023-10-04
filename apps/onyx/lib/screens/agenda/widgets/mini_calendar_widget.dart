0cadad0d69bbb5d33619b53898c7099dd203e906
eymeric <eymericdechelette@gmail.com>
2023-10-04 09:12:14 +0200

    refactor for optimisation

--- apps/onyx/lib/screens/agenda/widgets/mini_calendar_widget.dart
+++ apps/onyx/lib/screens/agenda/widgets/mini_calendar_widget.dart
@@ -45,101 +45,12 @@ class MiniCalendarWidget extends StatelessWidget {
                   ),
                 ),
               ),
-              for (int i = index; i < index + dayCount; i++) oneDay(context, i),
+              for (int i = index; i < index + dayCount; i++)
+                MiniCalendarOneDayWidget(context: context, currentDateIndex: i),
             ],
           );
         }
         return null;
-      },
-    );
-  }
-
-  Widget oneDay(BuildContext context, int currentDateIndex) {
-    final SettingsModel settings = context.read<SettingsCubit>().state.settings;
-    return BlocBuilder<AgendaCubit, AgendaState>(
-      buildWhen: (previous, current) =>
-          previous
-              .days(settings)[previous.wantedDate]
-              .date
-              .isSameDay(current.days(settings)[currentDateIndex].date) ||
-          current
-              .days(settings)[current.wantedDate]
-              .date
-              .isSameDay(current.days(settings)[currentDateIndex].date),
-      builder: (context, state) {
-        return SizedBox(
-          key: Key(
-              state.days(settings)[currentDateIndex].date.shrink(3).toString()),
-          height: Res.bottomNavBarHeight,
-          width: (100 - DaysViewRes.leftHourIndicatorWidth).w /
-              context.read<SettingsCubit>().state.settings.agendaWeekLength,
-          child: Padding(
-            padding: EdgeInsets.all(0.8.w),
-            child: Material(
-              borderRadius: BorderRadius.circular(10),
-              color: Colors.transparent,
-              child: AnimatedContainer(
-                duration: const Duration(milliseconds: 500),
-                decoration: BoxDecoration(
-                  color: (state.days(settings)[state.wantedDate].date.day ==
-                              state.days(settings)[currentDateIndex].date.day &&
-                          state.days(settings)[state.wantedDate].date.month ==
-                              state.days(settings)[currentDateIndex].date.month)
-                      ? Theme.of(context).primaryColor
-                      : Colors.transparent,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: InkWell(
-                  borderRadius: BorderRadius.circular(10),
-                  onTap: () {
-                    context.read<AgendaCubit>().updateDisplayedDate(
-                        wantedDate: currentDateIndex,
-                        fromMiniCalendar: true,
-                        settings: context.read<SettingsCubit>().state.settings,
-                        fromHorizontalScroll: false);
-                  },
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      SizedBox(
-                        height: 2.9.h,
-                        child: Text(
-                          state
-                              .days(settings)[currentDateIndex]
-                              .date
-                              .toMonthName(short: true),
-                          style: TextStyle(fontSize: 15.sp),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 3.h,
-                        child: Text(
-                          state
-                              .days(settings)[currentDateIndex]
-                              .date
-                              .day
-                              .toString(),
-                          style: TextStyle(fontSize: 17.sp),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 3.h,
-                        child: Text(
-                          state
-                              .days(settings)[currentDateIndex]
-                              .date
-                              .toWeekDayName(short: true),
-                          style: TextStyle(fontSize: 15.sp),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
-        );
       },
     );
   }
