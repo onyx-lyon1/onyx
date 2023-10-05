7df8ac2d4d371a73da62fc2e84743283732d3bff
eymeric <eymericdechelette@gmail.com>
2023-10-05 15:21:29 +0200

    don't rebuild for nothing

--- apps/onyx/lib/screens/map/widgets/custom_current_location_layer_widget.dart
+++ apps/onyx/lib/screens/map/widgets/custom_current_location_layer_widget.dart
@@ -10,11 +10,14 @@ class CustomCurrentLocationLayerWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<MapCubit, MapState>(builder: (context, state) {
-      if (state.geolocationAutorisation) {
-        return CurrentLocationLayer();
-      }
-      return Container();
-    });
+    return BlocBuilder<MapCubit, MapState>(
+        buildWhen: (previous, current) =>
+            previous.geolocationAutorisation != current.geolocationAutorisation,
+        builder: (context, state) {
+          if (state.geolocationAutorisation) {
+            return CurrentLocationLayer();
+          }
+          return Container();
+        });
   }
 }
