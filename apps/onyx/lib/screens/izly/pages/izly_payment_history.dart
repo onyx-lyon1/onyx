b42b175308257eb0dba67fd3900401e87e627165
eymeric <eymericdechelette@gmail.com>
2023-10-06 18:11:31 +0200

    use the izly cubit to listen to connection, also display better screen when loading (with header)

--- apps/onyx/lib/screens/izly/pages/izly_payment_history.dart
+++ apps/onyx/lib/screens/izly/pages/izly_payment_history.dart
@@ -10,86 +10,93 @@ class IzlyPaymentHistory extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: IzlyLogic.getUserPayments(
-          context.read<IzlyCubit>().state.izlyClient!),
-      builder: (context, state) {
-        if (state.hasData) {
-          List<PaymentModel> paymentsList = state.data!;
-          return SafeArea(
-            child: Material(
-              child: CommonScreenWidget(
-                header: const IzlyRechargeHeaderWidget(
-                    title: "Historique des paiements"),
-                body: Center(
-                  child: SingleChildScrollView(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        GridView.builder(
-                          gridDelegate:
-                              const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 1,
-                            childAspectRatio: 5,
-                          ),
-                          shrinkWrap: true,
-                          itemCount: paymentsList.length,
-                          itemBuilder: (BuildContext context, int index) {
-                            Color colorBackground;
-                            paymentsList[index].isSucess
-                                ? colorBackground = Colors.green
-                                : colorBackground = Colors.red;
+    return BlocBuilder<IzlyCubit, IzlyState>(
+      buildWhen: (previous, current) =>
+          previous.izlyClient != current.izlyClient,
+      builder: (context, izlyState) {
+        return FutureBuilder(
+          future: IzlyLogic.getUserPayments(izlyState.izlyClient!),
+          builder: (context, state) {
+            Widget body;
+            if (izlyState.izlyClient == null) {
+              body = const StateDisplayingPage(
+                message: "Vous n'êtes pas encore connecté à Izly",
+              );
+            } else if (!state.hasData) {
+              body = CircularProgressIndicator(
+                color: Theme.of(context).primaryColor,
+                strokeWidth: 7.sp,
+              );
+            } else {
+              body = SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    GridView.builder(
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 1,
+                        childAspectRatio: 5,
+                      ),
+                      shrinkWrap: true,
+                      itemCount: state.data!.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        Color colorBackground;
+                        state.data![index].isSucess
+                            ? colorBackground = Colors.green
+                            : colorBackground = Colors.red;
 
-                            return Container(
-                              decoration: BoxDecoration(
-                                color: colorBackground,
-                                borderRadius: BorderRadius.circular(12.0),
-                              ),
-                              margin: const EdgeInsets.all(8.0),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: <Widget>[
-                                  Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Text(
-                                      paymentsList[index].paymentTime,
-                                      style: const TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
+                        return Container(
+                          decoration: BoxDecoration(
+                            color: colorBackground,
+                            borderRadius: BorderRadius.circular(12.0),
+                          ),
+                          margin: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  state.data![index].paymentTime,
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
                                   ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Text(
-                                      paymentsList[index].amountSpent,
-                                      style: const TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                  )
-                                ],
+                                ),
                               ),
-                            );
-                          },
-                        ),
-                      ],
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  state.data![index].amountSpent,
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        );
+                      },
                     ),
+                  ],
+                ),
+              );
+            }
+            return SafeArea(
+              child: Material(
+                child: CommonScreenWidget(
+                  header: const IzlyRechargeHeaderWidget(
+                      title: "Historique des paiements"),
+                  body: Center(
+                    child: body,
                   ),
                 ),
               ),
-            ),
-          );
-        } else {
-          return Center(
-            child: CircularProgressIndicator(
-              color: Theme.of(context).primaryColor,
-              strokeWidth: 7.sp,
-            ),
-          );
-        }
+            );
+          },
+        );
       },
     );
   }
