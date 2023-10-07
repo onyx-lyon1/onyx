8b8e87476a20547d15ae9af066a0c7c05a83fcdd
eymeric <eymericdechelette@gmail.com>
2023-10-07 22:14:40 +0200

    better toMonthName

--- apps/onyx/lib/core/extensions/date_extension.dart
+++ apps/onyx/lib/core/extensions/date_extension.dart
@@ -1,6 +1,6 @@
 import 'package:onyx/core/extensions/extensions_export.dart';
 
-List<String> weekDays = [
+const List<String> weekDays = [
   "Lundi",
   "Mardi",
   "Mercredi",
@@ -9,7 +9,7 @@ List<String> weekDays = [
   "Samedi",
   "Dimanche"
 ];
-List<String> weekDaysShort = [
+const List<String> weekDaysShort = [
   "Lun.",
   "Mar.",
   "Mer.",
@@ -56,34 +56,7 @@ extension DateExtension on DateTime {
       short ? weekDaysShort[weekday - 1] : weekDays[weekday - 1];
 
   String toMonthName({bool short = false}) {
-    switch (month) {
-      case 1:
-        return short ? "Janv." : "Janvier";
-      case 2:
-        return short ? "Févr." : "Février";
-      case 3:
-        return short ? "Mars." : "Mars";
-      case 4:
-        return short ? "Avril" : "Avril";
-      case 5:
-        return short ? "Mai" : "Mai";
-      case 6:
-        return short ? "Juin" : "Juin";
-      case 7:
-        return short ? "Juil." : "Juillet";
-      case 8:
-        return short ? "Août" : "Août";
-      case 9:
-        return short ? "Sept." : "Septembre";
-      case 10:
-        return short ? "Oct." : "Octobre";
-      case 11:
-        return short ? "Nov." : "Novembre";
-      case 12:
-        return short ? "Déc." : "Décembre";
-      default:
-        return "Erreur";
-    }
+    return short ? monthNamesShort[month - 1] : monthNames[month - 1];
   }
 
   int toWeekNumber() {
