417e5ed376c790dc0afa4e7152791802a492c495
eymeric <eymericdechelette@gmail.com>
2023-10-08 15:07:22 +0200

    fix error probably caused by git

--- packages/lyon1mailclient/lib/src/config/config.dart
+++ packages/lyon1mailclient/lib/src/config/config.dart
@@ -1,11 +1,11 @@
 class Lyon1MailClientConfig {
   static const String base = "univ-lyon1.fr";
 
-  static const String imapHost = "accesbv.\\\$base";
+  static const String imapHost = "accesbv.$base";
   static const int imapPort = 993;
   static const bool imapSecure = true;
 
-  static const String smtpHost = "smtpbv.\\\$base";
+  static const String smtpHost = "smtpbv.$base";
   static const int smtpPort = 587;
   static const bool smtpSecure = false;
 
