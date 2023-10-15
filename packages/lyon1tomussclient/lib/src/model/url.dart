cfdfd56f8ab6a2508d95381bf1073f2fb60d097a
FlashOnFire_ <flashonfire.dev@gmail.com>
2023-10-15 17:27:49 +0200

    Fixed tomuss parser
    It couldn't extract the url when it was located in the "empty_is" field (why is it even here ??)

--- packages/lyon1tomussclient/lib/src/model/url.dart
+++ packages/lyon1tomussclient/lib/src/model/url.dart
@@ -10,9 +10,18 @@ class URL extends TeachingUnitElement {
   @HiveField(2, defaultValue: "")
   late final String value;
 
-  URL.fromJSON(var id, var json, var stats, var line, var column, String user)
+  URL.fromJSON(var id, Map json, var stats, var line, var column, String user)
       : super.fromJson(id, json, stats, line, column, user) {
-    value = line[id][0].toString();
+
+    List props =  line[id];
+    if (props.isNotEmpty) {
+      value = props[0].toString();
+    } else if (json.containsKey("empty_is")) {
+      value = json["empty_is"];
+    } else {
+      value = "";
+      print("Couldn't find url in URL object...");
+    }
   }
 
   URL({
