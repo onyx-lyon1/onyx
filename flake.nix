1f9f99ab958d2a26d584d34d295e5b4f29a26897
eymeric <eymericdechelette@gmail.com>
2024-01-18 18:09:49 +0100

    nix-unstable enfin ca maaarche ouhou

--- flake.nix
+++ flake.nix
@@ -1,9 +1,6 @@
 {
   inputs = {
-    #nixpkgs.url = "nixpkgs/nixos-unstable";
-    #nixpkgs.url = "github:hatch01/nixpkgs/master";
-    #nixpkgs.url = "github:FlafyDev/nixpkgs/flutter-from-source-5";
-    nixpkgs.url = "github:NixOs/nixpkgs/master";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
   
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
