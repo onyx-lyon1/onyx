8dd0f83be45d9300966bb2b2fb4fdfc4dd53c14b
eymeric <eymericdechelette@gmail.com>
2024-01-20 12:09:11 +0100

    better display sending email

--- apps/onyx/lib/screens/mails/states/email_cubit.dart
+++ apps/onyx/lib/screens/mails/states/email_cubit.dart
@@ -417,7 +417,9 @@ class EmailCubit extends Cubit<EmailState> {
         replyAll: replyAll,
         fromMailBox: (reply || forward) ? from : null);
     if (!Res.mock) {
-      mailClient!.addAction(action);
+      emit(state.copyWith(status: MailStatus.sending));
+      await mailClient!.addAction(action);
+      emit(state.copyWith(status: MailStatus.sended));
     }
   }
 
