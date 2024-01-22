63415eb16bea5dcaec54c5d5e4786ff8c0d25866
eymeric <eymericdechelette@gmail.com>
2024-01-22 16:16:46 +0100

    fix opening attachments

--- apps/onyx/lib/screens/mails/pages/email_details_page.dart
+++ apps/onyx/lib/screens/mails/pages/email_details_page.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -116,10 +117,14 @@ class MailDetailsPage extends StatelessWidget {
                                   //save data in a file and open it
                                   if (mail.attachmentsFiles
                                       .where((element) => element.path
-                                          .contains(mail.attachments[1]))
+                                          .contains(mail.attachments[index]))
                                       .isEmpty) {
-                                    mail.attachmentsFiles.insert(
-                                        index,
+                                    mail.attachmentsFiles.addAll(List.generate(
+                                        index -
+                                            mail.attachmentsFiles.length +
+                                            1,
+                                        (index) => File("")));
+                                    mail.attachmentsFiles[index] =
                                         await AttachmentLogic
                                             .getAttachmentLocalPath(
                                                 email: mail,
@@ -129,7 +134,7 @@ class MailDetailsPage extends StatelessWidget {
                                                 emailNumber: state.emailNumber,
                                                 fileName:
                                                     mail.attachments[index],
-                                                folder: state.currentMailBox!));
+                                                folder: state.currentMailBox!);
                                   }
                                   // ignore: use_build_context_synchronously
                                   showDialog(
