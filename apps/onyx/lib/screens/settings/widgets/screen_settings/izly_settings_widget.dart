bb3488354862223a10ce141036af7f9772b9018e
eymeric <eymericdechelette@gmail.com>
2024-01-24 11:59:09 +0100

    show warning on izly amount

--- apps/onyx/lib/screens/settings/widgets/screen_settings/izly_settings_widget.dart
+++ apps/onyx/lib/screens/settings/widgets/screen_settings/izly_settings_widget.dart
@@ -4,21 +4,44 @@ import 'package:izlyclient/izlyclient.dart';
 import 'package:onyx/core/cache_service.dart';
 import 'package:onyx/screens/izly/izly_export.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:onyx/screens/settings/domain/model/settings_model.dart';
+import 'package:onyx/screens/settings/states/settings_cubit.dart';
+import 'package:onyx/screens/settings/widgets/text_switch_widget.dart';
 
 class IzlySettingsWidget extends StatelessWidget {
   const IzlySettingsWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialButton(
-      minWidth: MediaQuery.of(context).size.width,
-      color: const Color(0xffbf616a),
-      textColor: Colors.white70,
-      child: Text(AppLocalizations.of(context).logoutIzly),
-      onPressed: () {
-        CacheService.reset<IzlyCredential>();
-        context.read<IzlyCubit>().disconnect();
-      },
+    return Column(
+      children: [
+        TextSwitchWidget(
+          text: AppLocalizations.of(context).enableIzlyNotif,
+          value: context.read<SettingsCubit>().state.settings.izlyNotification,
+          onChanged: (value) {
+            context.read<SettingsCubit>().modify(
+                  settings: context
+                      .read<SettingsCubit>()
+                      .state
+                      .settings
+                      .copyWith(izlyNotification: value),
+                );
+          },
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        MaterialButton(
+          minWidth: MediaQuery.of(context).size.width,
+          color: const Color(0xffbf616a),
+          textColor: Colors.white70,
+          child: Text(AppLocalizations.of(context).logoutIzly),
+          onPressed: () {
+            CacheService.reset<IzlyCredential>();
+            context.read<IzlyCubit>().disconnect();
+          },
+        ),
+      ],
     );
   }
 }
