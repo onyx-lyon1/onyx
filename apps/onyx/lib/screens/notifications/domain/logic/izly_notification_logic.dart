a5beab9f1a5ef1ce12c8d6c945eae1d3f203920d
VirisOnGithub <clement.reniers@etu.univ-lyon1.fr>
2024-01-24 22:03:27 +0100

    added notifications when izly amount is too low

--- apps/onyx/lib/screens/notifications/domain/logic/izly_notification_logic.dart
+++ apps/onyx/lib/screens/notifications/domain/logic/izly_notification_logic.dart
@@ -1,5 +1,31 @@
+import 'dart:async';
+
 import 'package:onyx/screens/settings/domain/model/settings_model.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:onyx/screens/notifications/notifications_export.dart';
+import 'package:onyx/screens/izly/izly_export.dart';
+import 'package:onyx/core/cache_service.dart';
 
 Future<void> izlyNotificationLogic(
-    SettingsModel settingsModel, AppLocalizations localizations) async {}
+    SettingsModel settings, AppLocalizations localizations) async {
+  if (settings.izlyNotification) {
+    if (await CacheService.exist<IzlyState>()) {
+      IzlyState state = (await CacheService.get<IzlyState>())!;
+      if (state.status == IzlyStatus.loaded) {
+        if (state.balance < 3.3) {
+          // Schedule notification for tomorrow at 11 AM
+          DateTime tomorrow = DateTime.now().add(const Duration(days: 1));
+          DateTime notificationTime =
+              DateTime(tomorrow.year, tomorrow.month, tomorrow.day, 11);
+          Duration delay = notificationTime.difference(DateTime.now());
+          Timer(delay, () {
+            NotificationLogic.showNotification(
+                title: localizations.izlyNotEnoughMoneyTitle,
+                body: localizations.izlyNotEnoughMoneyBody(state.balance),
+                payload: localizations.izlyNotEnoughMoneyTitle);
+          });
+        }
+      }
+    }
+  }
+}
