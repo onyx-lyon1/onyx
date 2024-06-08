5eb2799532b859523adefb9ab5617739ffdd0880
eymeric <eymericdechelette@gmail.com>
2024-06-08 22:02:47 +0200

    fix safe area for android smartphone

--- apps/onyx/lib/core/screens/home/pages/home_page.dart
+++ apps/onyx/lib/core/screens/home/pages/home_page.dart
@@ -7,7 +7,6 @@ import 'package:onyx/core/screens/home/home_export.dart';
 import 'package:onyx/core/widgets/core_widget_export.dart';
 import 'package:onyx/screens/login/login_export.dart';
 import 'package:onyx/screens/settings/settings_export.dart';
-import 'package:responsive_sizer/responsive_sizer.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -21,117 +20,123 @@ class HomePage extends StatefulWidget {
 class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<HomeCubit, HomeState>(
-      listener: (context, state) {
-        actionOnScreen(context, state.selectedIndex);
-      },
-      builder: (context, homeState) {
-        return BlocBuilder<AuthentificationCubit, AuthentificationState>(
-          builder: (context, authState) {
-            return BlocBuilder<SettingsCubit, SettingsState>(
-              buildWhen: (previous, current) {
-                if (!listEquals(previous.settings.enabledFunctionalities,
-                        current.settings.enabledFunctionalities) ||
-                    !listEquals(previous.settings.disabledFunctionalities,
-                        current.settings.disabledFunctionalities)) {
-                  //the page order has changer so we need to adapt the current page to avoir jump
+    return SafeArea(
+      child: BlocConsumer<HomeCubit, HomeState>(
+        listener: (context, state) {
+          actionOnScreen(context, state.selectedIndex);
+        },
+        builder: (context, homeState) {
+          return BlocBuilder<AuthentificationCubit, AuthentificationState>(
+            builder: (context, authState) {
+              return BlocBuilder<SettingsCubit, SettingsState>(
+                buildWhen: (previous, current) {
+                  if (!listEquals(previous.settings.enabledFunctionalities,
+                          current.settings.enabledFunctionalities) ||
+                      !listEquals(previous.settings.disabledFunctionalities,
+                          current.settings.disabledFunctionalities)) {
+                    //the page order has changer so we need to adapt the current page to avoir jump
 
-                  final previousIndex = homeState.selectedIndex;
-                  final functionalityName = [
-                    ...previous.settings.enabledFunctionalities,
-                  ][previousIndex]
-                      .name;
-                  final newEnabledIndex =
-                      current.settings.enabledFunctionalities.indexWhere(
-                          (element) => element.name == functionalityName);
-                  if (newEnabledIndex != -1) {
-                    context
-                        .read<HomeCubit>()
-                        .updateSelectedIndex(newEnabledIndex);
+                    final previousIndex = homeState.selectedIndex;
+                    final functionalityName = [
+                      ...previous.settings.enabledFunctionalities,
+                    ][previousIndex]
+                        .name;
+                    final newEnabledIndex =
+                        current.settings.enabledFunctionalities.indexWhere(
+                            (element) => element.name == functionalityName);
+                    if (newEnabledIndex != -1) {
+                      context
+                          .read<HomeCubit>()
+                          .updateSelectedIndex(newEnabledIndex);
+                    }
+                    return true;
                   }
-                  return true;
-                }
-                return false;
-              },
-              builder: (context, settingState) {
-                final enabledFunctionalities =
-                    settingState.settings.enabledFunctionalities;
+                  return false;
+                },
+                builder: (context, settingState) {
+                  final enabledFunctionalities =
+                      settingState.settings.enabledFunctionalities;
 
-                List<Destination> enabledDestinations = [];
-                for (var i = 0; i < enabledFunctionalities.length; i++) {
-                  enabledDestinations.add(Destination(
-                      i,
-                      enabledFunctionalities[i].name,
-                      enabledFunctionalities[i].toIcon()));
-                }
+                  List<Destination> enabledDestinations = [];
+                  for (var i = 0; i < enabledFunctionalities.length; i++) {
+                    enabledDestinations.add(Destination(
+                        i,
+                        enabledFunctionalities[i].name,
+                        enabledFunctionalities[i].toIcon()));
+                  }
 
-                // add the more button
-                if (enabledDestinations.length > 4) {
-                  enabledDestinations.insert(
-                      3,
-                      Destination(
-                          enabledFunctionalities.length,
-                          AppLocalizations.of(context).more,
-                          Icons.more_horiz_rounded));
-                }
-                return Scaffold(
-                  backgroundColor: Theme.of(context).colorScheme.surface,
-                  resizeToAvoidBottomInset: false,
-                  body: Expanded(
-                    child: CommonScreenWidget(
-                        onRefresh: () async {},
-                        state: (context
-                                    .read<AuthentificationCubit>()
-                                    .state
-                                    .status ==
-                                AuthentificationStatus.authentificating)
-                            ? LoadingHeaderWidget(
-                                message:
-                                    AppLocalizations.of(context).casConnexion)
-                            : null,
-                        body: Stack(
-                          children: [
-                            SizedBox(
-                              height: 100.h - 80,
-                              child: Stack(
-                                fit: StackFit.expand,
-                                children: enabledDestinations
-                                    .map((page) {
-                                      var index = page.index;
-                                      if (index >=
-                                          enabledFunctionalities.length) {
-                                        return null;
-                                      }
-                                      final view = enabledFunctionalities[index]
-                                          .toPage();
-                                      //Using offstage to avoid the hidden pages to rebuild
-                                      if (index == homeState.selectedIndex) {
-                                        return Offstage(
-                                            offstage: false, child: view);
-                                      } else {
-                                        return Offstage(child: view);
-                                      }
-                                    })
-                                    .whereType<
-                                        Widget>() //little hack to drop null values
-                                    .toList(),
-                              ),
-                            ),
-                            Align(
-                              alignment: Alignment.bottomCenter,
-                              child: BottomNavBarWidget(
-                                enabledDestinations: enabledDestinations,
-                              ),
-                            )
-                          ],
-                        )),
-                  ),
-                );
-              },
-            );
-          },
-        );
-      },
+                  // add the more button
+                  if (enabledDestinations.length > 4) {
+                    enabledDestinations.insert(
+                        3,
+                        Destination(
+                            enabledFunctionalities.length,
+                            AppLocalizations.of(context).more,
+                            Icons.more_horiz_rounded));
+                  }
+                  return Scaffold(
+                    backgroundColor: Theme.of(context).colorScheme.surface,
+                    resizeToAvoidBottomInset: false,
+                    body: Expanded(
+                      child: CommonScreenWidget(
+                          onRefresh: () async {},
+                          state: (context
+                                      .read<AuthentificationCubit>()
+                                      .state
+                                      .status ==
+                                  AuthentificationStatus.authentificating)
+                              ? LoadingHeaderWidget(
+                                  message:
+                                      AppLocalizations.of(context).casConnexion)
+                              : null,
+                          body: LayoutBuilder(builder: (context, constraints) {
+                            return Stack(
+                              children: [
+                                SizedBox(
+                                  height: constraints.maxHeight - 80,
+                                  child: Stack(
+                                    fit: StackFit.expand,
+                                    children: enabledDestinations
+                                        .map((page) {
+                                          var index = page.index;
+                                          if (index >=
+                                              enabledFunctionalities.length) {
+                                            return null;
+                                          }
+                                          final view =
+                                              enabledFunctionalities[index]
+                                                  .toPage();
+                                          //Using offstage to avoid the hidden pages to rebuild
+                                          if (index ==
+                                              homeState.selectedIndex) {
+                                            return Offstage(
+                                                offstage: false, child: view);
+                                          } else {
+                                            return Offstage(child: view);
+                                          }
+                                        })
+                                        .whereType<
+                                            Widget>() //little hack to drop null values
+                                        .toList(),
+                                  ),
+                                ),
+                                Align(
+                                  alignment: Alignment.bottomCenter,
+                                  child: BottomNavBarWidget(
+                                    enabledDestinations: enabledDestinations,
+                                  ),
+                                )
+                              ],
+                            );
+                          })),
+                    ),
+                  );
+                },
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
