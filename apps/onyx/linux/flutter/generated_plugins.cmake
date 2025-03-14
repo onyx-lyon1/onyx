485debbcd46e23cadf397431e0628433e894eae4
eymeric <eymericdechelette@gmail.com>
2025-03-14 16:53:12 +0100

    chore: update Android SDK and Kotlin versions; add file selector plugin support

--- apps/onyx/linux/flutter/generated_plugins.cmake
+++ apps/onyx/linux/flutter/generated_plugins.cmake
@@ -5,6 +5,7 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   biometric_storage
   desktop_window
+  file_selector_linux
   url_launcher_linux
 )
 
--- apps/onyx/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/onyx/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,12 +8,14 @@ import Foundation
 import biometric_storage
 import connectivity_plus
 import desktop_window
+import file_picker
 import flutter_local_notifications
 import geolocator_apple
 import path_provider_foundation
+import quill_native_bridge_macos
 import screen_brightness_macos
 import share_plus
-import sqflite
+import sqflite_darwin
 import url_launcher_macos
 import webview_flutter_wkwebview
 
@@ -21,12 +23,14 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   BiometricStorageMacOSPlugin.register(with: registry.registrar(forPlugin: "BiometricStorageMacOSPlugin"))
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   DesktopWindowPlugin.register(with: registry.registrar(forPlugin: "DesktopWindowPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  QuillNativeBridgePlugin.register(with: registry.registrar(forPlugin: "QuillNativeBridgePlugin"))
   ScreenBrightnessMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenBrightnessMacosPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
-  FLTWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "FLTWebViewFlutterPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
