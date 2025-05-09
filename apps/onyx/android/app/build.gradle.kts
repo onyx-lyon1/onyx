846c67a6c543e0052be208fb03af11a070d5cb7b
eymeric <eymericdechelette@gmail.com>
2025-05-09 11:43:17 +0200

    chore: upgrade android versions and goodby build.gradle

--- apps/onyx/android/app/build.gradle.kts
+++ apps/onyx/android/app/build.gradle.kts
@@ -7,10 +7,11 @@ plugins {
 
 android {
     namespace = "com.example.onyx"
-    compileSdk = 34
+    compileSdk = 35
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -21,13 +22,14 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.onyx"
+        applicationId = "fr.onyx.lyon1"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled = true
     }
 
     buildTypes {
@@ -41,4 +43,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
