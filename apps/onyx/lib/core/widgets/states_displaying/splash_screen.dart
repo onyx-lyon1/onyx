6110466e375a9321b86c50dd3ca5bab9531eb378
eymeric <eymericdechelette@gmail.com>
2025-05-11 20:52:38 +0200

    refactor: simplify splash screen animation by removing TweenAnimationBuilder

--- apps/onyx/lib/core/widgets/states_displaying/splash_screen.dart
+++ apps/onyx/lib/core/widgets/states_displaying/splash_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:onyx/core/res.dart';
 import 'package:onyx/l10n/app_localizations.dart';
@@ -15,57 +13,30 @@ class SplashScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TweenAnimationBuilder<Offset>(
-              tween: Tween<Offset>(
-                begin: Offset(0, 10.w),
-                end: Offset.zero,
+            SizedBox(
+              height: 40.w,
+              width: 40.w,
+              child: Image.asset(Res.iconPath),
+            ),
+            TweenAnimationBuilder<double>(
+              tween: Tween<double>(
+                begin: 0,
+                end: 1,
               ),
               duration: Res.animationDuration,
               curve: Curves.easeInOut,
-              builder: (context, offset, child) {
-                return Transform.translate(
-                  offset: offset,
-                  child: Opacity(
-                    opacity: clampDouble(1 - offset.distance / 10.w, 0, 1),
-                    child: SizedBox(
-                      height: 40.w,
-                      width: 40.w,
-                      child: Image.asset(Res.iconPath),
+              builder: (context, opacity, child) {
+                return Opacity(
+                  opacity: opacity,
+                  child: Text(
+                    AppLocalizations.of(context).onyx,
+                    style: TextStyle(
+                      fontSize: 20.sp,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
                 );
               },
-            ),
-            SizedBox(
-              height: 30.sp,
-              child: FutureBuilder(
-                future: Future.delayed(Res.animationDuration),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState != ConnectionState.done) {
-                    return const SizedBox.shrink();
-                  }
-                  return TweenAnimationBuilder<double>(
-                    tween: Tween<double>(
-                      begin: 0,
-                      end: 1,
-                    ),
-                    duration: Res.animationDuration,
-                    curve: Curves.easeInOut,
-                    builder: (context, opacity, child) {
-                      return Opacity(
-                        opacity: opacity,
-                        child: Text(
-                          AppLocalizations.of(context).onyx,
-                          style: TextStyle(
-                            fontSize: 20.sp,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      );
-                    },
-                  );
-                },
-              ),
             ),
           ],
         ),
