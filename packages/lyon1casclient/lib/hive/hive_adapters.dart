59dcf3643b3455f2d84ba0d5cbf53ffe87913835
eymeric <eymericdechelette@gmail.com>
2025-08-30 20:06:24 +0200

    feat: ditch hive in casclient

--- packages/lyon1casclient/lib/hive/hive_adapters.dart
+++
@@ -1,9 +0,0 @@
-import 'package:hive_ce/hive.dart';
-import 'package:lyon1casclient/src/credential.dart';
-
-part 'generated/hive_adapters.g.dart';
-
-@GenerateAdapters([
-  AdapterSpec<Credential>(),
-])
-class HiveAdapters {}
