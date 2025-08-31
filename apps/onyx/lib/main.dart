0209b6a31512ef177197f91c018988ab8e111882
eymeric <eymericdechelette@gmail.com>
2025-08-31 21:34:53 +0200

    Move Lyon1MailClient.initSembast to main entrypoint

--- apps/onyx/lib/main.dart
+++ apps/onyx/lib/main.dart
@@ -6,6 +6,8 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lyon1mailclient/lyon1mailclient.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:onyx/app.dart';
 import 'package:onyx/core/bloc_logger.dart';
 import 'package:onyx/core/initialisations/initialisations_export.dart';
@@ -29,6 +31,7 @@ void main() async {
   }
 
   await initSembastDb();
+  Lyon1MailClient.initSembast(path: await getApplicationDocumentsDirectory().then((value) => value.path));
 
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
