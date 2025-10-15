b76e65251e0c23de1a5e5008413b0f599c50e370
Tim&O <timeogodefroy@gmail.com>
2025-10-15 09:04:19 +0200

    feat: AngendaConfigPage go back in resources tree

--- apps/onyx/lib/screens/agenda_config/page/agenda_config_page.dart
+++ apps/onyx/lib/screens/agenda_config/page/agenda_config_page.dart
@@ -60,12 +60,27 @@ class AgendaConfigPage extends StatelessWidget {
                   }
                 },
                 child: PopScope(
-                  onPopInvokedWithResult: (_, _) async {
-                    pageController.animateToPage(
-                      pageController.page!.toInt() - 1,
-                      duration: Res.animationDuration,
-                      curve: Curves.easeInOut,
-                    );
+                  canPop: false,
+                  onPopInvokedWithResult: (didPop, _) async {
+                    if (didPop) {
+                      return ;
+                    } else {
+                      if (((state.status == AgendaConfigStatus.loaded ||
+                            state.status == AgendaConfigStatus.chosen) &&
+                          state.expandedResources.isNotEmpty) ||
+                          state.status == AgendaConfigStatus.searchResult) {
+                        context
+                            .read<AgendaConfigCubit>()
+                            .collapseResource(state.expandedResources.last);
+                      } else {
+                        pageController.animateToPage(
+                          pageController.page!.toInt() - 1,
+                          duration: Res.animationDuration,
+                          curve: Curves.easeInOut,
+                        );
+                        Navigator.of(context).pop();
+                      }
+                    }
                   },
                   child: Stack(
                     alignment: Alignment.bottomCenter,
@@ -106,7 +121,7 @@ class AgendaConfigPage extends StatelessWidget {
                       BlocBuilder<AgendaConfigCubit, AgendaConfigState>(
                         builder: (context, state) {
                           return AnimatedScale(
-                            scale: state.choosedIds.isNotEmpty ? 1 : 0,
+                            scale: state.chosenIds.isNotEmpty ? 1 : 0,
                             duration: Res.animationDuration,
                             child: Container(
                               margin: EdgeInsets.only(bottom: 10.h),
@@ -129,7 +144,7 @@ class AgendaConfigPage extends StatelessWidget {
                               child: IconButton(
                                 onPressed: () {
                                   context.read<AgendaConfigCubit>().onBack(
-                                    state.choosedIds,
+                                    state.chosenIds,
                                   );
                                 },
                                 icon: Icon(
@@ -209,7 +224,18 @@ class AgendaConfigPage extends StatelessWidget {
                                 if (state.status ==
                                     AgendaConfigStatus.searchResult) {
                                   context.read<AgendaConfigCubit>().unSearch();
+                                } else if ((state.status == AgendaConfigStatus.loaded ||
+                                    state.status == AgendaConfigStatus.chosen) &&
+                                    state.expandedResources.isNotEmpty) {
+                                  context
+                                      .read<AgendaConfigCubit>()
+                                      .collapseResource(state.expandedResources.last);
                                 } else {
+                                  pageController.animateToPage(
+                                    pageController.page!.toInt() - 1,
+                                    duration: Res.animationDuration,
+                                    curve: Curves.easeInOut,
+                                  );
                                   Navigator.of(context).pop();
                                 }
                               },
