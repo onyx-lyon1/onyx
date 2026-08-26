12cd8970affe182e06d459e79b2d8910a8a4c7f9
eymeric <hatchchien@protonmail.com>
2026-08-26 14:41:25 +0700

    feat: update minor

--- apps/onyx/lib/core/encrypt_codec.dart
+++ apps/onyx/lib/core/encrypt_codec.dart
@@ -3,10 +3,29 @@ import 'dart:math';
 import 'dart:typed_data';
 
 import 'package:crypto/crypto.dart';
-import 'package:encrypt/encrypt.dart';
-
+import 'package:path/path.dart' as p;
+import 'package:pointycastle/export.dart';
 // ignore: implementation_imports
 import 'package:sembast/src/api/v2/sembast.dart';
+
+/// Salsa 20 encoding using pointycastle
+class Salsa20 {
+  final Uint8List key;
+
+  Salsa20(this.key);
+
+  Uint8List _process(Uint8List input, Uint8List iv, bool forEncryption) {
+    var params = ParametersWithIV<KeyParameter>(KeyParameter(key), iv);
+    var cipher = Salsa20Engine();
+    cipher.init(forEncryption, params);
+    return cipher.process(input);
+  }
+
+  Uint8List encrypt(Uint8List input, Uint8List iv) => _process(input, iv, true);
+
+  Uint8List decrypt(Uint8List input, Uint8List iv) =>
+      _process(input, iv, false);
+}
 
 final _random = () {
   try {
@@ -51,16 +70,16 @@ class _EncryptEncoder extends Converter<Object?, String> {
   _EncryptEncoder(this.salsa20);
 
   @override
-  String convert(dynamic input) {
-    // Generate random initial value
+  String convert(Object? input) {
+    // Generate random initial value (nonce for Salsa20 is 8 bytes)
     final iv = _randBytes(8);
     final ivEncoded = base64.encode(iv);
     assert(ivEncoded.length == 12);
 
     // Encode the input value
-    final encoded = Encrypter(
-      salsa20,
-    ).encrypt(json.encode(input), iv: IV(iv)).base64;
+    final inputBytes = utf8.encode(json.encode(input));
+    final encryptedBytes = salsa20.encrypt(Uint8List.fromList(inputBytes), iv);
+    final encoded = base64.encode(encryptedBytes);
 
     // Prepend the initial value
     return '$ivEncoded$encoded';
@@ -74,16 +93,18 @@ class _EncryptDecoder extends Converter<String, Object?> {
   _EncryptDecoder(this.salsa20);
 
   @override
-  dynamic convert(String input) {
+  Object? convert(String input) {
     // Read the initial value that was prepended
     assert(input.length >= 12);
     final iv = base64.decode(input.substring(0, 12));
 
     // Extract the real input
-    input = input.substring(12);
+    final encryptedBytes = base64.decode(input.substring(12));
 
     // Decode the input
-    var decoded = json.decode(Encrypter(salsa20).decrypt64(input, iv: IV(iv)));
+    final decryptedBytes = salsa20.decrypt(encryptedBytes, iv);
+    var decoded = json.decode(utf8.decode(decryptedBytes));
+
     if (decoded is Map) {
       return decoded.cast<String, Object?>();
     }
@@ -97,7 +118,7 @@ class _EncryptCodec extends Codec<Object?, String> {
   late _EncryptDecoder _decoder;
 
   _EncryptCodec(Uint8List passwordBytes) {
-    var salsa20 = Salsa20(Key(passwordBytes));
+    var salsa20 = Salsa20(passwordBytes);
     _encoder = _EncryptEncoder(salsa20);
     _decoder = _EncryptDecoder(salsa20);
   }
@@ -141,6 +162,9 @@ SembastCodec getEncryptSembastCodec({required String password}) => SembastCodec(
 /// Wrap a factory to always use the codec
 class EncryptedDatabaseFactory implements DatabaseFactory {
   final DatabaseFactory databaseFactory;
+
+  @override
+  p.Context get pathContext => databaseFactory.pathContext;
   late final SembastCodec codec;
 
   EncryptedDatabaseFactory({
--- apps/onyx/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/onyx/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,13 +11,13 @@ import desktop_window
 import file_picker
 import flutter_local_notifications
 import geolocator_apple
-import path_provider_foundation
 import quill_native_bridge_macos
 import screen_brightness_macos
 import share_plus
 import sqflite_darwin
 import url_launcher_macos
 import webview_flutter_wkwebview
+import workmanager_apple
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   BiometricStorageMacOSPlugin.register(with: registry.registrar(forPlugin: "BiometricStorageMacOSPlugin"))
@@ -26,11 +26,11 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   QuillNativeBridgePlugin.register(with: registry.registrar(forPlugin: "QuillNativeBridgePlugin"))
   ScreenBrightnessMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenBrightnessMacosPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
+  WorkmanagerPlugin.register(with: registry.registrar(forPlugin: "WorkmanagerPlugin"))
 }
